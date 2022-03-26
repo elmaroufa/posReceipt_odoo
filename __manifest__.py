85a09d267678a27ff618428f1d8a50c7cc0b09a7
elmaroufa <elmaroufa@gmail.com>
2022-03-26 14:45:48 +0100

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'Receipt Caisse',
+	'summary': 'Customized Receipt of Point Of Sales',
+	'version': '13.0',
+	'author': 'Proxima Technologie Cameroon',
+	'category': 'Point Of Sale',
+	'depends': [
+		'base', 
+		'point_of_sale',
+		],
+	'data': [],
+	'demo': [],
+	'qweb': ['static/src/xml/pos.xml'],
+	'installable': True,
+	'auto_install': False,
+	'application': True,
+}
